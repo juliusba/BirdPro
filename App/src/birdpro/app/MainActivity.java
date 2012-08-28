9ef1a2134825b023586a5aedf171e6212edc04bf
Jonas Amundsen <JonasBA@Gmail.com>
2012-08-28 15:06:33 +0200

    Test.

--- App/src/birdpro/app/MainActivity.java
+++ App/src/birdpro/app/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+	// Dette er en kommentar.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
