3f64816e0062ef85e15d06e8867c61b228bbd079
Julius Asplin <julius_asplin@hotmail.com>
2012-09-03 14:24:01 +0200

    en liten start... laget en del klasser.. skal lage et greit logisk view asap så det er lett å sette seg inn i, om det ikke er det allerede.

--- App/gen/birdpro/app/R.java
+++ App/gen/birdpro/app/R.java
@@ -10,29 +10,50 @@ package birdpro.app;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int light=0x7f040000;
+    }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int bird=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_rec=0x7f020003;
+        public static final int rec_button_normal=0x7f020004;
+        public static final int rec_button_normal2=0x7f020005;
+        public static final int rec_button_pressed=0x7f020006;
+        public static final int rec_button_pressed2=0x7f020007;
+        public static final int rec_start=0x7f020008;
+        public static final int rec_start_normal=0x7f020009;
+        public static final int rec_start_pressed=0x7f02000a;
+        public static final int rec_stop=0x7f02000b;
+        public static final int rec_stop2=0x7f02000c;
+        public static final int rec_stop_normal=0x7f02000d;
+        public static final int rec_stop_pressed=0x7f02000e;
+        public static final int smooth_rectangle=0x7f02000f;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070003;
-        public static final int ratingBar1=0x7f070002;
-        public static final int textView1=0x7f070000;
-        public static final int toggleButton1=0x7f070001;
+        public static final int fragment_container=0x7f080000;
+        public static final int menu_settings=0x7f080003;
+        public static final int recButton=0x7f080001;
+        public static final int trackList=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int recorder_view=0x7f030001;
+        public static final int track_list_view=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int rec_button_start=0x7f050004;
+        public static final int rec_button_stop=0x7f050005;
+        public static final int title_activity_main=0x7f050003;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
--- App/src/birdpro/app/MainActivity.java
+++ App/src/birdpro/app/MainActivity.java
@@ -1,24 +1,32 @@
 package birdpro.app;
 
 import android.os.Bundle;
-import android.app.Activity;
+
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.Window;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
-	// Dette er en kommentar.
-    	//Min kommentar er sterkere ennd din
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        
+        FragmentManager fragmentManager = getSupportFragmentManager();
+		FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+		
+		RecorderFragment recorderFragment = new RecorderFragment();
+		fragmentTransaction.add(R.id.fragment_container, recorderFragment);
+		
+		TrackListFragment trackListFragment = new TrackListFragment();
+		fragmentTransaction.add(R.id.fragment_container, trackListFragment);
+		
+		fragmentTransaction.commit();
     }
-
-    //Testkommentar
-    //Testkommentar 2
-    //Testkommentar 3
-
-    
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
